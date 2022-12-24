ca1141204372714640e9a66fc3634bdadb1f3cf2
Your Name <you@example.com>
2022-12-24 21:43:37 +0530

    First

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:5000', 
+    const response = await fetch('https://codex-jwk2.onrender.com', 
     {
         method: 'POST',
         headers: {
